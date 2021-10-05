5cd14fde8599dbb9f423d1d547e9461893d2c114
DESKTOP-IEU8EOI\PC_USER <ryoheiotake@gmail.com>
2021-10-05 12:10:43 +0900

    bulma, jqueryを利用できるようにwebpack.mix.jsを書き換えた。

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-]);
+ mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/navbar.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .autoload({
+    jquery: ['$', 'window.jQuery']
+});
+
