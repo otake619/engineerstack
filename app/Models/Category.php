16a336f00f3ae8550ed5b91842fa7e2ca3dca29f
DESKTOP-IEU8EOI\PC_USER <ryoheiotake@gmail.com>
2021-10-07 07:31:53 +0900

    カテゴリのモデル、コントローラーのstore(DBへのinsert)を実装。

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category',
+        'user_id',
+        'memo_id',
+        'created_user_id',
+        'updated_user_id'
+    ]
+
+    public static function store(string $category, int $user_id, int $memo_id)
+    {
+        $memo = Memo::create([
+            'category' => $category,
+            'user_id' => $user_id,
+            'memo_id' => $memo_id,
+            'created_user_id' => $user_id
+        ]);
+    }
 }
