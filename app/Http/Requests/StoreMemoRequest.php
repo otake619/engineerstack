6465a2540f5a8d901636ce9be689083a37ef45d2
DESKTOP-IEU8EOI\PC_USER <ryoheiotake@gmail.com>
2021-10-07 05:36:18 +0900

    MemoのFormRequestに条件を追加。 カテゴリは各カテゴリ最大30文字かつ必須、タイトルは最大100文字かつ必須、メモはjsonかつ必須で 条件を指定した。

--- app/Http/Requests/StoreMemoRequest.php
+++ app/Http/Requests/StoreMemoRequest.php
@@ -13,7 +13,7 @@ class StoreMemoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,18 @@ class StoreMemoRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'categories' => 'required|array',
+            'categories.*' => 'nullable|max:30'
+            'title' => 'required|max:100',
+            'memo' => 'required|json'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'categories.required' => 'カテゴリは必ず1つの入力が必要です',
+            'categories.*.max:30' => '各カテゴリは最大30文字です'
         ];
     }
 }
