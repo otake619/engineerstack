8fa0863d7ce4b9f4bfa28f6aa298bd5f6fd45879
DESKTOP-IEU8EOI\PC_USER <ryoheiotake@gmail.com>
2021-10-25 17:37:31 +0900

    CategoryServiceクラスにRegisterCategoriesメソッドを追加。

--- app/Services/CategoriesService.php
+++ app/Services/CategoriesService.php
@@ -1,4 +1,14 @@
 <?php
     class CategoriesService {
-        
+        /**
+         * メモ記録画面で入力されたカテゴリーを1つずつCategoryController
+         * へと渡す。
+         * @param 
+         * string: $categories
+         * @return void
+         */
+        public function RegisterCategories($categories)
+        {
+            
+        }
     }
