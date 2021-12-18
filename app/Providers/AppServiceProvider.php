019f1e56f1f2e6fa5d567357265f016f9ccaaca8
DESKTOP-IEU8EOI\PC_USER <ryoheiotake@gmail.com>
2021-12-18 21:44:50 +0900

    procfileを修正。

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
         }
     }
 }
