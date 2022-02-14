ef37ebc178fc1fd447652e21e74680e86d9fed34
otake619 <ryoheiotake@gmail.com>
2022-02-14 20:30:09 +0900

    Google search console用にメタタグを設定。

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="6Z83FzF_pB3AY2FWDQHOU5hvrGqRQLR8TfOruIae2T0" />
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>アカウント作成 EngineerStack</title>
 </head>
